67b1c1f25398d67e60012c986155e8d9a932e269
Laura Pigott <pigottlaura@gmail.com>
2016-01-28 13:34:52 +0000

    Adding comments

--- app-a.js
+++ app-a.js
@@ -1,6 +1,12 @@
+// Accessing the File System Module and storing it so I can
+// access the methods of it later
 var fs = require("fs");
 
+// Using the File System Module to read in the contents of the
+// current directory
 fs.readdir("./", function(err, fileDir){
+  // Looping through each of the files returned from the directory
+  // and logging out their filename
   for(f in fileDir){
     console.log(fileDir[f]);
   }
