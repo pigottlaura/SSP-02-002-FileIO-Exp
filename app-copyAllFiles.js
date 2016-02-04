a843be2851d287907148f60768a794b20c5b5b1e
Laura Pigott <pigottlaura@gmail.com>
2016-02-04 18:10:06 +0000

    Successfully copying files
    Successfully copying all files (except those that I have chosen to ignore i.e. JavaScript files). Currently, these copies are simply files being read in, and written out to a new file, so must find out if this count's as 'copying' a file

--- app-copyAllFiles.js
+++ app-copyAllFiles.js
@@ -2,21 +2,6 @@
 // access the methods of it later
 var fs = require("fs");
 
-// Creating an array to store all the files I read in, aswell as their
-// data, so that they can be all printed out together in order at the
-// end of them all being read in
-var allFiles = [];
-
-// Creating a constructor for a FileTemplate object, so that each file that
-// is read in will store the same name value pairs (the filename, the contents of
-// the file, the order in which it was requested and the order in which is was returned)
-var FileTemplate = function(filename, fileContents, fileOrderNumber, fileReturnedAt){
-  this.name = filename;
-  this.contents = fileContents;
-  this.orderNum = fileOrderNumber;
-  this.returnedNum = fileReturnedAt;
-};
-
 // Creating an array of filenames or types that I would like to
 // ignore i.e. I do not want to access the contents of any .js
 // files as it would just result in me logging out the code
@@ -27,9 +12,6 @@ var ignoreFiles = [
   ".git",
   "search-results"
 ];
-
-var filesRead = 0;
-var totalIgnoredFiles = 0;
 
 // Using the File System Module to read in the contents of the
 // current directory
@@ -48,33 +30,16 @@ fs.readdir("./", function(err, fileDir){
       // Reading in the contents of the current file, and logging then out to
       // the console
       fs.readFile(file, "utf8", function(err, data){
-        // Creating a temporary instance of the FileTemplate object,
-        // which will store the filename, contents and index (of when this file
-        // was requested) in it, so that it can then be passed to and stored
-        // in the allFiles array. This is so I can store every files data,
-        // and then print them all out at the one time
-        var tempFile = new FileTemplate(file, data, index, filesRead);
 
-        // Pushing the temporary object into the allFiles array
-        allFiles.push(tempFile);
+        fs.writeFile("COPY-" + file, data, function(err) {
+          if(err)
+          {
+              console.log("\nERROR - file not copied: " + err);
+          } else {
+            console.log("\n" + file + " has been successfully copied");
+          }
+        });
 
-        // Incrementing the number of files that have returned their data, so that
-        // I can later compare this number to the total files in the directory, excluding
-        // the ones we ignored using the totalIgnoredFiles variable, so I know when all
-        // files have returned their data
-        filesRead++;
-
-        // Checking if the number of files read is equal to the toal number of files
-        // that were in the directory, minus the ones that we ignored
-        if(filesRead == (fileDir.length - totalIgnoredFiles)){
-          // Logging out that all files have now been read in
-          console.log("All files have been read in \n");
-
-          // Calling the sort files function, to rearrange the order in which the files
-          // were stored in the array, to reflect the order in which they were requested,
-          // as opposed to the order in which they were returned
-          sortFiles();
-        }
       })
     }
   });
@@ -100,77 +65,10 @@ function checkIgnoreFiles(currentFilename){
       // If the currentFilename contains a filename or extension which I have chosen
       // to ignore, then allowFile is set to false i.e. this file will not be allowed
       allowFile = false;
-
-      // Incrementing the number of files that have been ignored, so that
-      // I can later compare this number to the total files in the directory, and
-      // the total number that have returned their data so far, so I know when all
-      // files have returned their data
-      totalIgnoredFiles++;
     }
   }
 
   // Returning a boolean value to say whether or not the currentFilename that was
   // passed in is going to be ignored or not i.e. do not read in any .js files
   return allowFile;
-}
-
-// Declaring the sort files function, to rearrange the order in which the files
-// were stored in the array, to reflect the order in which they were requested,
-// as opposed to the order in which they were returned
-function sortFiles(){
-
-  // Calling the sort method on the allFiles array, which contains an object for
-  // each file that was read in. Passing in a function so that I can carry out
-  // a custon sort. Giving this function two parametres, as each time the function
-  // gets called, the sort method will pass in two objects from the array, so I
-  // will us a and b to access the properties I want to sort by, and then compare
-  // them within the function
-  allFiles.sort(function(a, b){
-    // Creating a locally scoped variable, which will store the answer I want to return
-    // to the sort method. By default this answer will be set to 0, so that if the
-    // statement is false, no effect will occur on the array
-    var ans = 0;
-
-    if(a.orderNum < b.orderNum){
-      // If the orderNumber of a is less than the orderNumber of b, then return the value
-      // of 1, which will move a 1 place forward in the array, past b
-      ans = 1;
-    }
-    return ans;
-  });
-
-  // Calling the showContentsOfFiles funciton, to log out the results to the console (which
-  // are now in the order which they were requested)
-  showContentsOfFiles();
-}
-
-function showContentsOfFiles(){
-  // Looping through each of the files I read in earlier
-  for(f in allFiles){
-    // Logging out their filename, contents and the order in which they returned their results
-    console.log(allFiles[f].name + " contained: \n" + allFiles[f].contents + "\n which returned it's data in position " + allFiles[f].returnedNum + "\n");
-  }
-  saveToTxtFile();
-}
-
-function saveToTxtFile(){
-  // Creating a searchResults variable to store the string that I want
-  // to write to the search-results.txt file
-  var searchResults = "Asynchronous SEARCH RESULTS (in reverse order)- " + Date() + "\n\r";
-
-  for(al in allFiles)
-  {
-    // Storing the file data into the searchResults string, so it can be saved
-    // in the search-results.txt file
-    searchResults += allFiles[al].name + "\r" + "\t" + allFiles[al].contents + "\r\r";
-  }
-
-  fs.writeFile("search-results-app-readFilesInReverseOrder-aSync.txt", searchResults, function(err) {
-    if(err)
-    {
-        console.log('\nERROR - file not saved: ' + err);
-    } else {
-      console.log('\nYour search results have been saved');
-    }
-  });
 }
